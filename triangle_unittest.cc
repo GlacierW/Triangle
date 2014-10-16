afa6c9577041e652c88ab5b8942cd1793d8197d0
GlacierW <glacier314@gmail.com>
2014-10-16 20:17:29 +0800

    add testing on three types of triangle

--- triangle_unittest.cc
+++ triangle_unittest.cc
@@ -24,3 +24,19 @@ TEST( TriangleTest, NotTriangle ) {
 	EXPECT_EQ( get_triangle_type(3, 2, 1), TYPE_TRIANGLE_INVALID );
 	EXPECT_EQ( get_triangle_type(2, 3, 1), TYPE_TRIANGLE_INVALID );
 }
+
+TEST( TriangleTest, EquilateralTriange ) {
+	EXPECT_EQ( get_triangle_type(10, 10, 10), TYPE_TRIANGLE_EQUILATERAL );
+}
+
+TEST( TriangleTest, IsoscelesTriange ) {
+	EXPECT_EQ( get_triangle_type(10, 100, 100), TYPE_TRIANGLE_ISOSCELES );
+	EXPECT_EQ( get_triangle_type(100, 10, 100), TYPE_TRIANGLE_ISOSCELES );
+	EXPECT_EQ( get_triangle_type(100, 100, 10), TYPE_TRIANGLE_ISOSCELES );
+}
+
+TEST( TriangleTest, ScaleneTriange) {
+	EXPECT_EQ( get_triangle_type(10, 11, 12), TYPE_TRIANGLE_ISOSCELES );
+	EXPECT_EQ( get_triangle_type(12, 10, 11), TYPE_TRIANGLE_ISOSCELES );
+	EXPECT_EQ( get_triangle_type(11, 12, 10), TYPE_TRIANGLE_ISOSCELES );
+}
