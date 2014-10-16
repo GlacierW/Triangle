ebbeb47000ab84b8f6681b08501e44f0f438e748
GlacierW <glacier314@gmail.com>
2014-10-16 20:11:54 +0800

    add testing on negative edges

--- triangle_unittest.cc
+++ triangle_unittest.cc
@@ -1,4 +1,19 @@
 #include "triangle.h"
 #include <gtest/gtest.h>
 
+TEST( TriangleTest, NegativeEdge ) {
+	// one negative edge
+	EXPECT_EQ( get_triangle_type(-1, 1, 1), TYPE_TRIANGLE_INVALID );
+	EXPECT_EQ( get_triangle_type(1, -1, 1), TYPE_TRIANGLE_INVALID );
+	EXPECT_EQ( get_triangle_type(1, 1, -1), TYPE_TRIANGLE_INVALID );
+	
+	// two negative edges 
+	EXPECT_EQ( get_triangle_type(-1, -1, 1), TYPE_TRIANGLE_INVALID );
+	EXPECT_EQ( get_triangle_type(1, -1, -1), TYPE_TRIANGLE_INVALID );
+	EXPECT_EQ( get_triangle_type(-1, 1, -1), TYPE_TRIANGLE_INVALID );
+	
+	// all negative edges 
+	EXPECT_EQ( get_triangle_type(-1, -1, -1), TYPE_TRIANGLE_INVALID );
+}
+
 
